55ebea1df16941f7e3ac33240de70192b09d87ba
ejsadiarin <ejsadiarin@gmail.com>
2023-03-09 23:51:54 +0800

    added SearchBar component functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import LibraryBooks from "./components/LibraryBooks";
+import SearchBar from "./components/SearchBar";
 
 function App() {
 
   return (
     <div>
       <h1>ReactRead: A Book Searcher</h1>
+      {}
+      <SearchBar />
       <LibraryBooks />
     </div>
   );
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,31 @@
+import { useState } from "react"
+import data from '../data/booksData.json'
+import LibraryBooks from "./LibraryBooks";
 
 export default function SearchBar() {
+  const [input, setInput] = useState("");
+  const [isSearch, setSearch] = useState(false);
 
+  const handleSearch = () => {
+    setSearch(true);
+  }
+  const filteredBooks = data.filter(book => 
+    book.title.toLowerCase().includes(input.toLowerCase()));
+  
+    
 
   return (
     <div>
-      
+      <h1>Search Bar</h1>
+      <input type="text" value={input} onChange={e => setInput(e.target.value)} />
+      <button onClick={handleSearch}>Search</button>
+      {isSearch && filteredBooks.map(book => (
+        <div>
+          <h1>{book.title}</h1>
+          <h3>{book.author}</h3>
+          <p>{book.genre}</p>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
